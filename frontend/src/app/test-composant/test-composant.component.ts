46b368cf4749b06449a97be97e04e375e2fef388
zhak5388 <102757175+zhak5388@users.noreply.github.com>
2024-03-25 23:20:16 +0100

    Frontend : Connection with API 	angular.json 	proxy.conf.json 	app.config.ts (ProvideHttpClient)

--- frontend/src/app/test-composant/test-composant.component.ts
+++ frontend/src/app/test-composant/test-composant.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-test-composant',
   standalone: true,
   imports: [],
   templateUrl: './test-composant.component.html',
-  styleUrl: './test-composant.component.css'
 })
 export class TestComposantComponent {
+
+  // Don't forget to import provideHttpClient() in app.config.ts
+  constructor(private httpClient : HttpClient ) {
+  }
+  onSubmit(){
+    //Don't forget proxy.conf.json
+    this.httpClient.post("/api/player/add", {name: "jean"}).subscribe(
+      data => {console.log(data)},
+      error => {
+        console.log("There is an error: " + error)
+      }
+    )
+  }
 
 }
